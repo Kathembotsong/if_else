d6e048952fc34133af380debb2020842d50d0817
kathembotsongodieudonne <kathembo.dieudonne@gmail.com>
2023-09-22 15:38:44 +0300

    commit 2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    val cha = 'i'
+    val vowelConsonant = if (cha == 'a' || cha == 'e' || cha == 'i' || cha == 'o' || cha == 'u') "vowel" else "consonant"
+    println("$cha is $vowelConsonant")
+}
